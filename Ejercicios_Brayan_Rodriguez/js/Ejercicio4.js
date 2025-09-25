0ae64ee2a87a35ced2b182a615111a512f265a51
vjp-brayanRC <brodriguezc05@iesvjp.es>
2025-09-25 17:59:14 +0200

    ejercicio4

---
+++ Ejercicios_Brayan_Rodriguez/js/Ejercicio4.js
@@ -0,0 +1,7 @@
+// Crea una función que reciba 3 parámetros con valores por 
+// defecto (producto → "Producto genérico", precio → 100, 
+// impuestos → 21). La función convertiría las entradas a 
+// cadena, entero y entero. Si no se pudieran convertir las 
+// entradas, devolvería los valores por defecto. 
+// Prueba esta función varias veces, usando valores y poniendo 
+// valores incorrectos.
