12f5ae324849acd973937233f71f6580e9213f5d
vjp-brayanRC <brodriguezc05@iesvjp.es>
2025-10-09 17:51:44 +0200

    ejercicio 5 hacer el dos

--- prueba/ejercicio2/ejercicio2.js
+++ prueba/ejercicio2/ejercicio2.js
@@ -1,3 +1,4 @@
+ // 1, 2, 3
  let primerP = document.querySelector("a");
  let titulo = document.querySelector("div > a");
  let enlace = document.querySelector("#div1 > p > a");
@@ -8,3 +9,20 @@ console.log(primerP.title);
 console.log(titulo);
 console.log(enlace.textContent);
 console.log(elemento2.getAttribute("href"));
+
+// 8
+const spidermanElement = document.querySelector("a[title=Spiderman]");
+
+
+let elemento = spidermanElement;
+
+while(elemento = spidermanElement){
+    console.log(elemento.title);
+}
+
+elemento = spidermanElement;
+
+while(elemento = spidermanElement){
+    console.log(elemento.title);
+}
+
